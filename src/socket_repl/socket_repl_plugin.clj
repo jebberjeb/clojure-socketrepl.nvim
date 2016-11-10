fa2d2762ab89959d3bf1769a6b63eaef6337ba50
Jeb Beich <jebbeich@gmail.com>
2016-11-10 12:16:43 -0600

    Warn user if not connected to socket repl

--- src/socket_repl/socket_repl_plugin.clj
+++ src/socket_repl/socket_repl_plugin.clj
@@ -77,6 +77,21 @@
   []
   (swap! current-connection assoc :last (System/currentTimeMillis)))
 
+(defn warn-if-disconnect*
+  [connection f]
+  (fn [msg]
+    (if-not @connection
+      (nvim/vim-command-async
+        ":echo 'Use :Connect host:port to connect to a socket repl'"
+        (fn [_]))
+      (f msg))
+    :done))
+
+(defn warn-if-disconnect
+  "Warn the user if not connected to a socket repl."
+  [f]
+  (warn-if-disconnect* current-connection f))
+
 (defn wrap-eval
   "Wrap code in `eval`."
   [code-str]
@@ -141,64 +156,68 @@
                   (fn [x]
                     (write-output! x))))))
 
-   (nvim/register-method!
-     "eval-code"
-     (fn [msg]
-       (update-last!)
-       (nvim/get-cursor-location-async
-         (fn [coords]
-           (nvim/get-current-buffer-text-async
-             (fn [x]
-               (try
-                 (let [code (get-form-at x coords)]
-                   (write-output! (str code "\n"))
-                   (write-code! code ))
-                 (catch Throwable t
-                   ;; TODO: Use more general plugin-level ereror handling
-                   (write-output!
-                     (str "\n##### PLUGIN ERR #####\n"
-                          (.getMessage t) "\n"
-                          (string/join "\n" (map str (.getStackTrace t)))
-                          \n"######################\n"))))))))))
+  (nvim/register-method!
+    "eval-code"
+    (warn-if-disconnect
+      (fn [msg]
+        (update-last!)
+        (nvim/get-cursor-location-async
+          (fn [coords]
+            (nvim/get-current-buffer-text-async
+              (fn [x]
+                (try
+                  (let [code (get-form-at x coords)]
+                    (write-output! (str code "\n"))
+                    (write-code! code ))
+                  (catch Throwable t
+                    ;; TODO: Use more general plugin-level ereror handling
+                    (write-output!
+                      (str "\n##### PLUGIN ERR #####\n"
+                           (.getMessage t) "\n"
+                           (string/join "\n" (map str (.getStackTrace t)))
+                           \n"######################\n")))))))))))
 
-   (nvim/register-method!
-     "eval-buffer"
-     (fn [msg]
-       (update-last!)
-       (nvim/get-current-buffer-text-async
-         (fn [x]
-           (let [code (wrap-eval x)]
-             (write-output! (str code "\n"))
-             (write-code! code))))))
+  (nvim/register-method!
+    "eval-buffer"
+    (warn-if-disconnect
+      (fn [msg]
+        (update-last!)
+        (nvim/get-current-buffer-text-async
+          (fn [x]
+            (let [code (wrap-eval x)]
+              (write-output! (str code "\n"))
+              (write-code! code)))))))
 
-   (nvim/register-method!
-     "doc"
-     (fn [msg]
-       (nvim/get-current-word-async
-         (fn [word]
-           (let [code (format "(clojure.repl/doc  %s)" word)]
-             (write-output! (str code "\n"))
-             (write-code! code))))))
+  (nvim/register-method!
+    "doc"
+    (warn-if-disconnect
+      (fn [msg]
+        (nvim/get-current-word-async
+          (fn [word]
+            (let [code (format "(clojure.repl/doc  %s)" word)]
+              (write-output! (str code "\n"))
+              (write-code! code)))))))
 
-   (nvim/register-method!
-     "show-log"
-     (fn [msg]
-       (update-last!)
-       (let [file (-> @current-connection :file .getAbsolutePath)]
-         (go
-           (let [buffer-cmd (first (message/params msg))
-                 rlog-buffer (<! (get-rlog-buffer-async))
-                 rlog-buffer-visible? (when rlog-buffer
-                                        (<! (nvim/buffer-visible?-async
-                                              rlog-buffer)))]
-             (when-not rlog-buffer-visible?
-               (nvim/vim-command-async
-                 (format "%s | nnoremap <buffer> q :q<cr> | :let b:rlog=1 | :call termopen('tail -f %s')"
-                         buffer-cmd file)
-                 (fn [_])))))
-         ;; Don't return a core.async channel, else msgpack will fail to
-         ;; serialize it.
-         "success")))
+  (nvim/register-method!
+    "show-log"
+    (warn-if-disconnect
+      (fn [msg]
+        (update-last!)
+        (let [file (-> @current-connection :file .getAbsolutePath)]
+          (go
+            (let [buffer-cmd (first (message/params msg))
+                  rlog-buffer (<! (get-rlog-buffer-async))
+                  rlog-buffer-visible? (when rlog-buffer
+                                         (<! (nvim/buffer-visible?-async
+                                               rlog-buffer)))]
+              (when-not rlog-buffer-visible?
+                (nvim/vim-command-async
+                  (format "%s | nnoremap <buffer> q :q<cr> | :let b:rlog=1 | :call termopen('tail -f %s')"
+                          buffer-cmd file)
+                  (fn [_])))))
+          ;; Don't return a core.async channel, else msgpack will fail to
+          ;; serialize it.
+          "success"))))
 
   ;; Don't need to do this in debug, socket repl will keep this alive.
   (when-not debug
