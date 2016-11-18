d5a01707e208fcfb2440e1830c704495bed37fd1
Jeb Beich <jebbeich@gmail.com>
2016-11-18 10:51:19 -0600

    Add rlwrap to debug script

--- tmux-run-dev.sh
+++ tmux-run-dev.sh
@@ -8,9 +8,9 @@ fi
 tmux new-window -n 'nvim-debug'
 tmux send-keys "NVIM_LISTEN_ADDRESS=127.0.0.1:7777 nvim -S plugin/socketrepl.vim.debug" C-m
 tmux split
-tmux send-keys 'exec java -cp "$(cat .classpath)" clojure.main' C-m
+tmux send-keys 'rlwrap java -cp "$(cat .classpath)" clojure.main' C-m
 tmux send-keys '(println "Plugin repl connecting to nvim")' C-m
 tmux send-keys '(go)' C-m
 tmux split
-tmux send-keys 'exec java -cp "$(cat .classpath)" -Dclojure.server.repl="{:port 5555 :accept clojure.core.server/repl :server-daemon false}" clojure.main' C-m
+tmux send-keys 'rlwrap java -cp "$(cat .classpath)" -Dclojure.server.repl="{:port 5555 :accept clojure.core.server/repl :server-daemon false}" clojure.main' C-m
 tmux send-keys '(println "Socket repl server on 5555")' C-m
